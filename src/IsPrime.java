b5794d608899af08446c6bbafdc449401b2537c7
deepvn98 <deepvn988@gmail.com>
2021-04-04 18:06:47 +0700

    baitap

---
+++ src/IsPrime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class IsPrime {
+    static boolean isPrime( int number){
+        if ( number < 2 ){
+          return false;
+        }
+        for ( int i = 2; i <= Math.sqrt(number); i++ ){
+            if (number % i == 0 ){
+              return false;
+            }
+        }return true;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Mời bạn nhập số cần kiểm tra: ");
+        int number = scan.nextInt();
+        if (isPrime(number)){
+            System.out.println(number+ " là số nguyên tố!");
+        }else {
+            System.out.println(number+" không phải là số nguyên tố!");
+        }
+
+    }
+}
