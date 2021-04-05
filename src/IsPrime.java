c16c05557838ea404a67675a92efacc22b5276c4
deepvn98 <deepvn988@gmail.com>
2021-04-05 09:04:35 +0700

    baitap

--- src/IsPrime.java
+++ src/IsPrime.java
@@ -11,17 +11,28 @@ public class IsPrime {
             }
         }return true;
     }
+    static void Showall(int n){
+        int count = 0;
+        int number = 2;
+        while (count<=n){
+            if (isPrime(number))
+            System.out.println(number+ ",");
+            count++;
+            number++;
+        }
+    }
 
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Mời bạn nhập số cần kiểm tra: ");
-        int number = scan.nextInt();
-        if (isPrime(number)){
-            System.out.println(number+ " là số nguyên tố!");
-        }else {
-            System.out.println(number+" không phải là số nguyên tố!");
-        }
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Mời bạn nhập số cần kiểm tra: ");
+//        int number = scan.nextInt();
+//        if (isPrime(number)){
+//            System.out.println(number+ " là số nguyên tố!");
+//        }else {
+//            System.out.println(number+" không phải là số nguyên tố!");
+//        }
+        Showall(20);
 
     }
 }
